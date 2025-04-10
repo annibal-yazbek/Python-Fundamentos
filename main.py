32deff3c1428eca9a422e4782758598ef2f34711
Annibal <annibal.yazbek@icloud.com>
2025-04-10 11:23:25 -0300

    Atualizando

--- main.py
+++
@@ -1,6 +0,0 @@
-from sys import path
-
-path.append('C:\\Users\\yazbe\\OneDrive\\Documentos\\Annibal\\Treinamento-Cursos - coursera\\Python+Docker+ML-Udemy-MaykoSilva\\PCEP')
-
-import extra.good.best.sierra
-print(extra.good.best.sierra.FunS())
---
+++ main2.py
@@ -0,0 +1,6 @@
+from sys import path
+
+path.append('C:\\Users\\yaz\\OneDrive\\Documentos\\An\\Treinamentos\\Python+Docker\\Fundamentos')
+
+import extra.good.best.sierra
+print(extra.good.best.sierra.FunS())
