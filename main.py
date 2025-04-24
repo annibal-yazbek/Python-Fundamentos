1a32b545843f98c0fee854ed166eb889a355c5fe
Annibal <annibal.yazbek@icloud.com>
2025-04-24 10:12:51 -0300

    web scraping 2

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ r = requests.get(url)
 conteudoHtml = r.text
 htmlSoup = BeautifulSoup(conteudoHtml, features="lxml")
 print(htmlSoup.find('h1'))
+for f in htmlSoup.find_all(['h1', 'h2']):
+    print(f)
 
